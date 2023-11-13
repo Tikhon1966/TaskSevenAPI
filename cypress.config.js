b9544c74470955b07a8492619b1273bbb20cbe8d
Oleg Tikhomirov <ovt1966@gmail.com>
2023-11-13 14:05:45 +0300

    Add CypressCloud

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,7 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  projectId: 'tqwjds',
   e2e: {
     baseUrl: "https://petstore.swagger.io/v2",
     setupNodeEvents(on, config) {
